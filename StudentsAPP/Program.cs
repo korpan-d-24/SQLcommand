941282cd766ad76be8279f451ac083a4f52290ff
korpan-d-24 <korpan24denis@gmail.com>
2022-01-25 15:06:17 +0200

    SQL command

--- StudentsAPP/Program.cs
+++ StudentsAPP/Program.cs
@@ -28,6 +28,9 @@ namespace StudentsAPP
 
             while(true)
             {
+                try
+                {
+
                 Console.Write("> ");
                 command = Console.ReadLine();
                 #region Exit
@@ -45,7 +48,46 @@ namespace StudentsAPP
                 }
                 #endregion
 
+                SqlCommand sqlCommand = new SqlCommand(command, sqlConnection);
 
+                // SELECT * FROM [Students] WHERE Id=1
+
+                switch(command.Split(' ')[0].ToLower())
+                {
+                    case "select":
+                        sqlDataReader = sqlCommand.ExecuteReader();
+                        while(sqlDataReader.Read())
+                        {
+                            Console.WriteLine($"{sqlDataReader["Id"]} {sqlDataReader["FullName"]}" +
+                                $"{sqlDataReader["Birthday"]} {sqlDataReader["University"]} {sqlDataReader["Group_Num"]}" +
+                                $"{sqlDataReader["Course"]} {sqlDataReader["Avarge_score"]}");
+
+                            Console.WriteLine(new string('-', 30));
+                        }
+                        if (sqlDataReader != null)
+                        {
+                            sqlDataReader.Close();
+                        }
+                        break;
+                    case "insert":
+                        Console.WriteLine($"Added: {sqlCommand.ExecuteNonQuery()} string(s)");
+                        break;
+                    case "update":
+                        Console.WriteLine($"Changet: {sqlCommand.ExecuteNonQuery()} string(s)");
+                        break;
+                    case "delete":
+                        Console.WriteLine($"Deleted: {sqlCommand.ExecuteNonQuery()} string(s)");
+                        break;
+                    default:
+                        Console.WriteLine($"Commant {command} isn`t correct!");
+                        break;
+                }
+
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine($"Mistake {ex.Message}");
+                }
             }
 
             Console.WriteLine("Pres any button to continue");
