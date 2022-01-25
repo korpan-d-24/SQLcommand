a69dafa4ab31baef63f5eb51d8465383c1944d9c
korpan-d-24 <korpan24denis@gmail.com>
2022-01-25 15:49:54 +0200

    'sortby'

--- StudentsAPP/Program.cs
+++ StudentsAPP/Program.cs
@@ -48,13 +48,20 @@ namespace StudentsAPP
                 }
                 #endregion
 
-                SqlCommand sqlCommand = new SqlCommand(command, sqlConnection);
+                if(command.ToLower().Equals("clear"))
+                    {
+                        Console.Clear();
+                        continue;
+                    }
+                SqlCommand sqlCommand = null;
 
-                // SELECT * FROM [Students] WHERE Id=1
-
-                switch(command.Split(' ')[0].ToLower())
+                    // SELECT * FROM [Students] WHERE Id=1
+                    string[] commandArray = command.ToLower().Split(' ');
+                
+                switch(commandArray[0])
                 {
                     case "select":
+                        sqlCommand = new SqlCommand(command, sqlConnection);
                         sqlDataReader = sqlCommand.ExecuteReader();
                         while(sqlDataReader.Read())
                         {
@@ -70,15 +77,34 @@ namespace StudentsAPP
                         }
                         break;
                     case "insert":
+                        sqlCommand = new SqlCommand(command, sqlConnection);
                         Console.WriteLine($"Added: {sqlCommand.ExecuteNonQuery()} string(s)");
                         break;
                     case "update":
+                        sqlCommand = new SqlCommand(command, sqlConnection);
                         Console.WriteLine($"Changet: {sqlCommand.ExecuteNonQuery()} string(s)");
                         break;
                     case "delete":
+                        sqlCommand = new SqlCommand(command, sqlConnection);
                         Console.WriteLine($"Deleted: {sqlCommand.ExecuteNonQuery()} string(s)");
                         break;
-                    default:
+                        case "sortby":
+                            sqlCommand = new SqlCommand($"SELECT * FROM [Table] ORDER BY {commandArray[1]} {commandArray[2]}", sqlConnection);
+                            sqlDataReader = sqlCommand.ExecuteReader();
+                            while (sqlDataReader.Read())
+                            {
+                                Console.WriteLine($"{sqlDataReader["Id"]} {sqlDataReader["FullName"]}" +
+                                    $"{sqlDataReader["Birthday"]} {sqlDataReader["University"]} {sqlDataReader["Group_Num"]}" +
+                                    $"{sqlDataReader["Course"]} {sqlDataReader["Avarge_score"]}");
+
+                                Console.WriteLine(new string('-', 30));
+                            }
+                            if (sqlDataReader != null)
+                            {
+                                sqlDataReader.Close();
+                            }
+                            break;
+                        default:
                         Console.WriteLine($"Commant {command} isn`t correct!");
                         break;
                 }
